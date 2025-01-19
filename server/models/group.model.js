ece2c1c2b3379c8664eda9718a988b82d9a1e438
jasmine lo <jasminelo@Jasmines-MBP-3.attlocal.net>
2025-01-19 01:55:31 -0800

    update group model

--- server/models/group.model.js
+++ server/models/group.model.js
@@ -1,7 +1,32 @@
 const mongoose = require("mongoose");
 
 const GroupSchema = mongoose.Schema(
-	{},
+	{
+		owner: {
+			type: String,
+			required: true,
+		},
+
+		users: {
+			type: Array,
+			required: true,
+		},
+
+		groupCode: {
+			type: Number,
+			required: true,
+		},
+
+		bobas: {
+			type: Array,
+			required: false,
+		},
+
+		groupPFP: {
+			type: String,
+			required: false,
+		},
+	},
 	{
 		timestamps: true,
 	}
